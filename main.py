0818bfe91d5b324a144f870a9a7918f457350c90
Mr.D <51441065+volk1234@users.noreply.github.com>
2023-09-25 16:22:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ hostname = "0.0.0.0"
 port = 8080
 
 if __name__ == "__main__":
-    sleep(30)
+
     server = HTTPServer((hostname, port), SimpleServer)
     print("HTTP server started at: http://%s:%s" % (hostname, port))
 
